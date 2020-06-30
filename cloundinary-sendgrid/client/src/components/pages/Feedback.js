5c3ed102fa2e2123f1f2b07d789bd96b57c47d38
juniordugue <jrdugue@gmail.com>
2020-06-30 16:23:37 -0400

    Passed messaged as a parameter via my onChange handler, created two more label fields, one for email address and another for phone numbers, and added a dynamic submit button and added bootstrap class form control to the input tags

--- cloundinary-sendgrid/client/src/components/pages/Feedback.js
+++ cloundinary-sendgrid/client/src/components/pages/Feedback.js
@@ -35,23 +35,52 @@ const Feedback = () => {
         <div className="form-group">
           <label className="text-muted">Description</label>
           <textarea
-            onChange={handleChange}
+            onChange={handleChange("message")}
             type="text"
             className="form-control"
+            value={message}
             required
           ></textarea>
         </div>
         <div className="form-group">
           <label className="text-muted">Your Name</label>
-          <input type="text" onChange={handleChange("name")} />
+          <input 
+          className="form-control"
+            type="text"
+            onChange={handleChange("name")}
+            value={name}
+            required
+          />
         </div>
+        <div className="form-group">
+          <label className="text-muted">Your Email</label>
+          <input 
+          className="form-control"
+            type="email"
+            onChange={handleChange("email")}
+            value={email}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label className="text-muted">Your Phone</label>
+          <input 
+          className="form-control"
+            type="number"
+            onChange={handleChange("phone")}
+            value={phone}
+            required
+          />
+        </div>
+
+        <button className="btn btn-outline-primary btn-block">{buttonText}</button>
       </form>
     </>
   );
 
   return (
-    <div>
-      <h2>Feedback App</h2>
+    <div className="p-5">
+      {FeedbackForm()}
     </div>
   );
 };
